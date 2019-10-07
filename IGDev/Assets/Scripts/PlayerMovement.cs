19afbaf556369030edb6818c7ea362b0a51ddde2
Walter <granck.fate@gmail.com>
2019-10-07 18:00:43 +1100

    Scene and PlayerMovement
    player moves around scene, and flips.

---
+++ IGDev/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        //GetAxis returns -1.0 - +1.0; Based on input.
+        pos.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+
+        //Update Position
+        transform.position = pos;
+        FlipSprite();
+    }
+
+    void FlipSprite()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        if ((mousePos.x - transform.position.x) < 0.0f)
+        {
+            sprite.flipX = true;
+        }
+        if ((mousePos.x - transform.position.x) > 0.0f)
+        {
+            sprite.flipX = false;
+        }
+    }
+}
---
+++ IGDev/Assets/Scripts/StaffRotation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaffRotation : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    bool faceLeft = false;
+    bool faceRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeRotation();
+        FlipSprite();
+    }
+
+    void ChangeRotation()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+
+        Debug.Log(mousePos.x - transform.position.x + "mouseX");
+        Debug.Log(mousePos.y - transform.position.y + "mouseY");
+
+        transform.up = direction;
+    }
+
+    void FlipSprite()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        if ((mousePos.x - transform.position.x) < 0.0f)
+        {
+            sprite.flipX = true;
+            faceLeft = true;
+            faceRight = false;
+            //transform.position = new Vector3(transform.position.x - 0.35f, transform.position.y, transform.position.z);
+        }
+        if ((mousePos.x - transform.position.x) > 0.0f)
+        {
+            sprite.flipX = false;
+            faceLeft = false;
+            faceRight = true;
+        }
+    }
+}
